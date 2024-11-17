14b88f24188a34f7467f04b71b15b13a39626ac9
martijnlammers <m.b.alyanak@gmail.com>
2024-11-17 16:37:11 +0100

    fixed error

--- src/debug_functions.py
+++ src/debug_functions.py
@@ -7,10 +7,10 @@ from rectangle import Rectangle
 
 def show_detection(frame: Image, rectangle: Rectangle):
 
-    top_left = rectangle.top_left
-    bottom_right = rectangle.bottom_right
+    cv2.rectangle(frame.data, rectangle.top_left(), rectangle.bottom_right(), const.RGB_RED, const.BOX_THICKNESS)
 
-    cv2.rectangle(frame.data, top_left, bottom_right, const.RGB_RED, const.BOX_THICKNESS)
-    cv2.putText(frame.data, 'Detection', rectangle.bottom_right, const.FONT, const.FONT_SCALE, const.RGB_RED)
+    top_left_point = rectangle.top_left()
+    text_origin = (top_left_point[0], top_left_point[1] - 10)
+    cv2.putText(frame.data, str(rectangle.top_left()), text_origin, const.FONT, const.FONT_SCALE, const.RGB_RED)
     cv2.imshow("Detection Verification", frame.data)
     cv2.waitKey()
