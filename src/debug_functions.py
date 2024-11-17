d64ad05b1293fa7280a18c61e304bf1063eec901
martijnlammers <m.b.alyanak@gmail.com>
2024-11-17 16:16:32 +0100

    renamed self to s

--- src/debug_functions.py
+++ src/debug_functions.py
@@ -5,9 +5,12 @@ from image import Image
 from rectangle import Rectangle
 
 
-def show_detection(self, frame: Image, rectangle: Rectangle):
+def show_detection(frame: Image, rectangle: Rectangle):
 
-    cv2.rectangle(frame.data, rectangle.top_left, rectangle.bottom_right, const.RGB_RED, const.BOX_THICKNESS)
+    top_left = rectangle.top_left
+    bottom_right = rectangle.bottom_right
+    
+    cv2.rectangle(frame.data, top_left, bottom_right, const.RGB_RED, const.BOX_THICKNESS)
     cv2.putText(frame.data, 'Detection', rectangle.bottom_right, const.FONT, const.FONT_SCALE, const.RGB_RED)
     cv2.imshow("Detection Verification", frame.data)
     cv2.waitKey()
